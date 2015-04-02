c8a49fd104b980db11f7d536ff0eb384189b8235
Josh Kramer <jkra@bn.co>
2015-04-02 13:12:13 -0400

    Move directive into folder, make template, replace:true

--- scripts/jk-question-answer/jk-question-answer.js
+++ scripts/jk-question-answer/jk-question-answer.js
@@ -3,6 +3,8 @@ angular.module('demoApp').directive('jkQuestionAnswer', function() {
 	return {
 		restrict: 'E',
 		scope: false,
+		replace: true,
+		templateUrl: 'jk-question-answer/jk-question-answer.html',
 		link: function (scope, element) {
 			console.log('inside jk-question-answer directive');
 		}
