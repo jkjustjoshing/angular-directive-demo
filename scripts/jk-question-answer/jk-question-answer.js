99e6470f9093bbd1e5c14893eaefb5dd8f1ee1e4
Josh Kramer <jkra@bn.co>
2015-04-02 13:16:32 -0400

    Replace false instead of “more than one root” error (see console error).

--- scripts/jk-question-answer/jk-question-answer.js
+++ scripts/jk-question-answer/jk-question-answer.js
@@ -3,7 +3,7 @@ angular.module('demoApp').directive('jkQuestionAnswer', function() {
 	return {
 		restrict: 'E',
 		scope: false,
-		replace: true,
+		replace: false,
 		// Must run from server for AJAX to work! http://joshkra.me/angular-directive-demo
 		templateUrl: 'scripts/jk-question-answer/jk-question-answer.html',
 		link: function (scope, element) {
