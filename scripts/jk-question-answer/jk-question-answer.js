f46b9613b786c3eedf793b2c2107b405fa862a74
Josh Kramer <jkra@bn.co>
2015-04-02 13:19:45 -0400

    Make scope isolate

--- scripts/jk-question-answer/jk-question-answer.js
+++ scripts/jk-question-answer/jk-question-answer.js
@@ -2,12 +2,14 @@ angular.module('demoApp').directive('jkQuestionAnswer', function() {
 
 	return {
 		restrict: 'E',
-		scope: false,
+		scope: {
+			data: '='
+		},
 		replace: false,
 		// Must run from server for AJAX to work! http://joshkra.me/angular-directive-demo
 		templateUrl: 'scripts/jk-question-answer/jk-question-answer.html',
 		link: function (scope, element) {
-			console.log('inside jk-question-answer directive');
+			console.log(scope.data);
 		}
 	};
 
